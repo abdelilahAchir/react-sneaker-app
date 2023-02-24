9977f9fd89b2556200e06de75d7b0a20d6c0cfe4
Abdelilah Achir <abdiachir@gmail.com>
2023-02-24 14:34:49 +0100

    store users on firebase

--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -1,42 +1,36 @@
-import { loginRequest } from "../authConfig";
-import React, { useState } from "react";
+//import { loginRequest } from "../authConfig";
+import React from "react";
 import {  useMsal } from "@azure/msal-react";
-import Button from "react-bootstrap/Button";
-import { ProfileData } from "../components/ProfileData";
-import { callMsGraph } from "../Graph";
+//import { ProfileData } from "../components/ProfileData";
+//import { callMsGraph } from "../Graph";
 
 
  export function ProfileContent() {
-    const { instance, accounts } = useMsal();
-    const [graphData, setGraphData] = useState(null);
+    const {  accounts } = useMsal();
+   //const [graphData, setGraphData] = useState(null);
 
     const name = accounts[0] && accounts[0].name;
 
-    function RequestProfileData() {
-        const request = {
-            ...loginRequest,
-            account: accounts[0]
-        };
+    // function RequestProfileData() {
+    //     const request = {
+    //         ...loginRequest,
+    //         account: accounts[0]
+    //     };
+    //   //  Silently acquires an access token which is then attached to a request for Microsoft Graph data
+    //     instance.acquireTokenSilent(request).then((response) => {
+    //         callMsGraph(response.accessToken).then(response => setGraphData(response));
+    //     }).catch((e) => {
+    //         instance.acquireTokenPopup(request).then((response) => {
+    //             callMsGraph(response.accessToken).then(response => setGraphData(response));
+    //         });
+    //     });
 
-        // Silently acquires an access token which is then attached to a request for Microsoft Graph data
-        instance.acquireTokenSilent(request).then((response) => {
-            callMsGraph(response.accessToken).then(response => setGraphData(response));
-        }).catch((e) => {
-            instance.acquireTokenPopup(request).then((response) => {
-                callMsGraph(response.accessToken).then(response => setGraphData(response));
-            });
-        });
-    }
+       
+    // }
 
     return (
         <>
             <h5 className="card-title">Welcome {name}</h5>
-            {/* {graphData ? 
-           
-                <ProfileData graphData={graphData} />
-                :
-                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button> */}
-           
         </>
     );
 };
