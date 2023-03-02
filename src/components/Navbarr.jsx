b476e9cb550d9549548847c0f1d367069708a53c
Abdelilah Achir <abdiachir@gmail.com>
2023-03-02 21:53:46 +0100

    show cart quantity on navbar

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -2,47 +2,49 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faCartShopping, } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, } from '@fortawesome/free-solid-svg-icons'
 import './NavBar.css'
-import 'font-awesome/css/font-awesome.min.css'; 
-//import { useIsAuthenticated } from "@azure/msal-react";
+import 'font-awesome/css/font-awesome.min.css';
 import { SignInButton } from "./SingInButton";
 import { SignOutButton } from "./SignOutButton";
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
 import { ProfileContent } from "./ProfileContent";
+import ShoeContext from "../context/ShoeContext";
+import { useContext } from "react";
 
 
 
 export const Navbarr = (props) => {
 
-  //const isAuthenticated = useIsAuthenticated();
+  const itemsInCart = useContext(ShoeContext).itemsInCart;
+
 
   return (
     <>
-    <Container >
-    <Navbar bg="success" expand="lg">
-      <Container>
-        <Navbar.Brand href="/" className=''>Snkrs X u</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/sneakersXu">Shop</Nav.Link>
-            <Nav.Link href="/">Contact uZ</Nav.Link>
-            <Nav.Link href="/cart"><FontAwesomeIcon icon={faCartShopping}/></Nav.Link>
-          </Nav>
-          <UnauthenticatedTemplate>
-            <SignInButton/>
-            </UnauthenticatedTemplate>
-          <AuthenticatedTemplate>
-            <ProfileContent  />
-            <SignOutButton />
-          </AuthenticatedTemplate>
-        </Navbar.Collapse>
+      <Container >
+        <Navbar bg="success" expand="lg">
+          <Container>
+            <Navbar.Brand href="/" className=''>Snkrs X u</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/sneakersXu">Shop</Nav.Link>
+                <Nav.Link href="/">Contact uZ</Nav.Link>
+                <Nav.Link href="/cart"><FontAwesomeIcon icon={faCartShopping} /> {itemsInCart.length > 0 ? itemsInCart.length : ''}</Nav.Link>
+              </Nav>
+              <UnauthenticatedTemplate>
+                <SignInButton />
+              </UnauthenticatedTemplate>
+              <AuthenticatedTemplate>
+                <ProfileContent />
+                <SignOutButton />
+              </AuthenticatedTemplate>
+            </Navbar.Collapse>
+          </Container>
+
+        </Navbar>
+        {props.children}
       </Container>
-     
-    </Navbar>
-    {props.children}
-    </Container>
     </>
   );
 };
